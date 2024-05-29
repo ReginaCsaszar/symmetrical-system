d184183b084053eb9329c70b3b83e0913d1ba610
Regina Császár <khadris@hotmail.com>
2024-05-29 21:20:55 +0200

    error-free file copy

--- copy_target_files.ps1
+++ copy_target_files.ps1
@@ -1,25 +1,25 @@
 # Build mappa beállítása
 
 $scriptDir = $PWD.Path
-$opencvBuildDir = "$scriptDir\opencv\build-0527"
+$buildDir = "$scriptDir\opencv\build-0529-2"
 
 # A target mappák létrehozása
 
-$targetDir = Join-Path $scriptDir "target"
-$libDir = Join-Path $targetDir "lib"
-$opencv2Dir = Join-Path $targetDir "include\opencv2"
+$targetDir = "$scriptDir\target"
+$libDir = "$targetDir\lib"
+$cvDir = "$targetDir\include\opencv2"
 
-#mkdir -Path $targetDir
-#mkdir -Path $libDir
-#mkdir -Path $cv2Dir
+mkdir $targetDir
+mkdir $libDir
+mkdir $cvDir
 
 # A lib mappába másolás
-Copy-Item -Path "$opencvBuildDir\lib\libopencv_world.a" -Destination $libDir
-Copy-Item -Path "$opencvBuildDir\3rdparty\lib\*" -Destination $libDir -Recurse
+Copy-Item "$buildDir\lib\libopencv_world.a" -Destination $libDir
+Copy-Item "$buildDir\3rdparty\lib\*" -Destination $libDir -Recurse
 
 # Az opencv2 mappába másolás
-Copy-Item -Path "$opencvBuildDir\install\include\opencv4\opencv2\*" -Destination $opencv2Dir -Recurse
-Copy-Item -Path .\opencv\include\opencv2\* -Destination $opencv2Dir -Recurse
-Copy-Item -Path "$opencvBuildDir\opencv2\*" -Destination $opencv2Dir -Recurse
+Copy-Item "$buildDir\install\include\opencv4\opencv2\*" -Destination $cvDir -Recurse
+Copy-Item .\opencv\include\opencv2\* -Destination $cvDir -Recurse
+Copy-Item "$buildDir\opencv2\*" -Destination $cvDir -Recurse
 
 Read-Host "Nyomjon meg egy billentyût a folytatáshoz..."
